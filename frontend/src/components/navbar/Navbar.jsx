01e672800cd1dda6dec855fd1713bf3d95b173d0
TKaburu <traceymwendwa@gmail.com>
2024-09-05 00:10:25 +0300

    styles: styling for the navigation component

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,23 +1,39 @@
-import React from 'react'
-import './navbar.css'
+import React, { useState } from 'react';
+import { FaBars, FaTimes } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+import './navbar.css';
 
 const Navbar = () => {
-  return (
-    <nav className="navbar">
-        <section className="logo">
-            PawPrints
-        </section>
-        <section className="links">
-            <a href="/">Home</a>
-            <a href="/about">Change Ownership</a>
-            <a href="/contact">Contact</a>
-        </section>
-        <section className='auth'>
-            <a href="/login">Login</a>
-            <a href="/register">Register</a>
-        </section>
-    </nav>
-  )
-}
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-export default Navbar
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  return (
+    <>
+    <nav className="navbar">
+      <section className="logo">
+        PawPrints
+      </section>
+      <section className={`links ${isMenuOpen ? 'open' : ''}`}>
+        <a href="/">Home</a>
+        <a href="/about">Change Ownership</a>
+        <a href="/contact">Contact</a>
+      </section>
+      <section className= {`links ${isMenuOpen ? 'open' : ''}`}>
+        <div className="auth">
+          <Link to="/login">Login</Link>
+          <button><Link to="/register" className='accent'>Register</Link></button>
+        </div>
+      </section>
+      
+      <div className="menu-icon" onClick={toggleMenu}>
+        {isMenuOpen ? <FaTimes /> : <FaBars />}
+      </div>
+    </nav>
+    </>
+  );
+};
+
+export default Navbar;
