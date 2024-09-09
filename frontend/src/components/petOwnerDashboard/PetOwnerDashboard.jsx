49438fe31f2c0b950e2da02f3f0cd5ae2ec7df41
TKaburu <traceymwendwa@gmail.com>
2024-09-09 13:57:11 +0300

    feat: dashboard for pet owners to see their pets

---
+++ frontend/src/components/petOwnerDashboard/PetOwnerDashboard.jsx
@@ -0,0 +1,65 @@
+import React, { useState, useEffect } from 'react';
+import api from '../../api/api';
+import { ACCESS_TOKEN } from '../../constants';
+import { useParams } from 'react-router-dom';
+
+const PetOwnerDashboard = () => {
+  const [pets, setPets] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const { username } = useParams();
+
+  useEffect(() => {
+    const fetchPets = async () => {
+      setLoading(true);
+      setError(null);
+
+      try {
+        const response = await api.get(`http://127.0.0.1:8000/auth/pet-owner-dashboard/${username}/`, {
+          headers: {
+            'Authorization': `Bearer ${localStorage.getItem(ACCESS_TOKEN)}`,
+          }
+        });
+        setPets(response.data);
+        setLoading(false);
+      } catch (error) {
+        console.error('Failed to fetch pets:', error);
+        setError('Failed to fetch pets. Please try again later.');
+        setLoading(false);
+      }
+    };
+
+    if (username) {
+      fetchPets();
+    }
+  }, [username]);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>{error}</p>;
+
+  return (
+    <div className='main-content'>
+      <section className="title">
+        <h1>Here are your registered fur babies</h1>
+      </section>
+      <section className="pet-list">  
+        {pets.map(Pet => (
+          <div className="pet-card" key={Pet.id}>
+            {/* <img src={Pet.image} alt={Pet.name} /> */}
+            <h2>{Pet.name}</h2>
+            <p>Type of Pet: {Pet.type_of_pet}</p>
+            <p>Breed: {Pet.breed}</p>
+            {/* <p>Date of Birth: {Pet.date_of_birth}</p>
+            <p>Gender: {Pet.gender}</p>
+            <p>Color: {Pet.color}</p>
+            <p>Weight: {Pet.weight}</p>
+            <p>Height: {Pet.height}</p> */}
+            
+          </div>
+        ))}
+    </section>
+    </div>
+  );
+};
+
+export default PetOwnerDashboard;
