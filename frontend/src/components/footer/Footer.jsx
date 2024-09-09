3deacc7db3d584c8fe4921d49f3254c0cb489c63
TKaburu <traceymwendwa@gmail.com>
2024-09-09 23:56:45 +0300

    feat: added link to register pet on the footer

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -4,23 +4,26 @@ import './footer.css'
 
 const Footer = () => {
   return (
-    <section className='footer'>
-        <section className='footer-content'>
-            <div className='contact'>
-                <h3>Contact</h3>
-                <p>123-456-7890</p>
-                <p> Email: email adreess </p>
-            </div>
-            <div className='quick-links'>
-                <h3>Quick Links</h3>
-                <a href="/about">Change Ownership</a>
-                <a href="/search">Microchip Search</a>
-                <a href="/contact">Contact</a>
-            </div>
-            
+    <div className='main-content'>
+      <section className='footer'>
+          <section className='footer-content'>
+              <div className='contact'>
+                  <h3>Contact</h3>
+                  <p>123-456-7890</p>
+                  <p> Email: email adreess </p>
+              </div>
+              <div className='quick-links'>
+                  <h3>Quick Links</h3>
+                  <Link to="/register-pet">Register a Pet</Link>
+                  <a href="/about">Change Ownership</a>
+                  <a href="/search">Microchip Search</a>
+                  <a href="/contact">Contact</a>
+              </div>
+              
 
-        </section>        
-    </section>
+          </section>        
+      </section>
+    </div>
   )
 }
 
