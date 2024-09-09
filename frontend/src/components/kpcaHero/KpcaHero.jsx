fa39425cded22946c276217f4639339d5b53bebf
TKaburu <traceymwendwa@gmail.com>
2024-09-09 23:57:10 +0300

    fix: corect KSPCA description

--- frontend/src/components/kpcaHero/KpcaHero.jsx
+++ frontend/src/components/kpcaHero/KpcaHero.jsx
@@ -6,15 +6,15 @@ const KpcaHero = () => {
   return (
     <section className="kpca-hero">
       <section className="kpca-details">
-      <section className="kpca-hero-image">
-        <a
-          href="https://www.kspca.or.ke/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img src={kpcaLogo} alt="KPCA Logo" />
-        </a>
-      </section>
+        <section className="kpca-hero-image">
+          <a
+            href="https://www.kspca.or.ke/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={kpcaLogo} alt="KPCA Logo" />
+          </a>
+        </section>
         <div className="title">
           <h3>
             A Proud Supporter of
@@ -22,22 +22,29 @@ const KpcaHero = () => {
               href="https://www.kspca.or.ke/"
               target="_blank"
               rel="noopener noreferrer"
-            ><span> KPCA Kenya</span></a>
-            
+            >
+              <span> KSPCA </span>
+            </a>
+            Kenya
           </h3>
         </div>
         <section className="kpca-description">
           <p>
-            The Kentucky Pet Care Association (KPCA) is a non-profit
-            organization that promotes the health and well-being of pets in
-            Kentucky. KPCA provides resources and support to pet owners, pet
-            care professionals, and animal shelters in the state. We are proud
-            to support KPCA in their mission to improve the lives of pets and
-            pet owners in Kentucky.
+            The Kenya Society for the Protection & Care of Animals (KSPCA) is an
+            animal welfare charity organization in Kenya that deals for the most
+            part with domestic animals. It is the only animal welfare
+            organization that runs an animal shelter in Kenya. The society has
+            branches in Nairobi, Mombasa and Naivasha.
           </p>
         </section>
       </section>
-      <a href="https://www.kspca.or.ke/donate-2-2/" target="_blank" rel="noopener noreferrer"><button className="donate-btn">Donate to KPCA</button></a>
+      <a
+        href="https://www.kspca.or.ke/donate-2-2/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <button className="donate-btn">Donate to KPCA</button>
+      </a>
     </section>
   );
 };
