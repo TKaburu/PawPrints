35eb5414bc8ffde99fb1e485e2bbb9c3f4521b67
TKaburu <traceymwendwa@gmail.com>
2024-12-11 10:26:23 +0300

    fix: changed accounts urls from auth

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('rest_framework.urls')),
     path('api/', include('api.urls')),
-    path('auth/', include('accounts.urls')),
+    path('accounts/', include('accounts.urls')),
 ]
