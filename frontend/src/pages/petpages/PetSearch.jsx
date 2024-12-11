3024f026049932433058b711f50302036a3fd8c2
TKaburu <traceymwendwa@gmail.com>
2024-12-11 13:07:26 +0300

    feat: added message if search is done without micro chip number

--- frontend/src/pages/petpages/PetSearch.jsx
+++ frontend/src/pages/petpages/PetSearch.jsx
@@ -10,6 +10,12 @@ const PetSearch = () => {
 
   const handleSearch = async () => {
     setSearched(true); // Set searched to true when search is performed
+    
+    if (!microchipNo) {
+      setError('Please enter a valid microchip number.');
+      return;
+    }
+    
     try {
       const data = await searchPetByMicrochip(microchipNo);
       console.log("API Response:", data);
@@ -27,50 +33,55 @@ const PetSearch = () => {
   };
 
   return (
-    <section className="search-container">
-      <div className="title"> 
-        <h1>Search Pet By Microchip</h1>
-      </div>
-      <section className="search-bar">
-        <input
-          type="text"
-          value={microchipNo}
-          onChange={(e) => setMicrochipNo(e.target.value)}
-          placeholder="Enter microchip number"
-        />
-      </section>
-      <div className="search-btn">
-        <button onClick={handleSearch}>Search</button>
-      </div>
-      <section className={`search-results ${searched ? "show-results" : ""}`}>
-        {error && <p>{error}</p>}
-        {result && (
-          <div className="pet-results">
-            <div className="title">
-              <h2>Pet Details</h2>
-            </div>
-            <section className="pet-result-details">
-              {result.map((pet, slug) => (
-                <div key={slug}>
-                  <section className="pet-details">
-                    <p>Name: {pet.name}</p>
-                    <p>Owner: {pet.pet_parent_name.first_name}</p>
-                    <p>Primary Vet: {pet.primary_vet.first_name} {pet.primary_vet.last_name}</p>
-                    <p>Contact: {pet.primary_vet_contact}</p>
+    <section className="main-container">
+      <section className="search-container">
+        <div className="title"> 
+          <h1>Search Pet By Microchip</h1>
+        </div>
+        <section className="description">
+          <p>Enter a microchip number down bellow to find the petowner</p>
+        </section>
+        <section className="search-bar">
+          <input
+            type="text"
+            value={microchipNo}
+            onChange={(e) => setMicrochipNo(e.target.value)}
+            placeholder="Enter microchip number"
+          />
+        </section>
+        <div className="search-btn">
+          <button onClick={handleSearch}>Search</button>
+        </div>
+        <section className={`search-results ${searched ? "show-results" : ""}`}>
+          {error && <p>{error}</p>}
+          {result && (
+            <div className="pet-results">
+              <div className="title">
+                <h2>Pet Details</h2>
+              </div>
+              <section className="pet-result-details">
+                {result.map((pet, slug) => (
+                  <div key={slug}>
+                    <section className="pet-details">
+                      <p>Name: {pet.name}</p>
+                      <p>Owner: {pet.pet_parent_name.first_name}</p>
+                      <p>Primary Vet: {pet.primary_vet.first_name} {pet.primary_vet.last_name}</p>
+                      <p>Contact: {pet.primary_vet_contact}</p>
 
-                    {pet.secondary_vet && (
-                      <>
-                        <p>Secondary Vet: {pet.secondary_vet.first_name} {pet.secondary_vet.last_name}</p>
-                        <p>Contact: {pet.secondary_vet_contact}</p>
-                      </>
-                    )}
-                  </section>
-                  
-                </div>
-              ))}
-            </section>
-          </div>
-        )}
+                      {pet.secondary_vet && (
+                        <>
+                          <p>Secondary Vet: {pet.secondary_vet.first_name} {pet.secondary_vet.last_name}</p>
+                          <p>Contact: {pet.secondary_vet_contact}</p>
+                        </>
+                      )}
+                    </section>
+                    
+                  </div>
+                ))}
+              </section>
+            </div>
+          )}
+        </section>
       </section>
     </section>   
   );
