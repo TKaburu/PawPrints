69a8d2c64f022e7862d55b50773c4ee7268240b0
TKaburu <traceymwendwa@gmail.com>
2024-12-12 13:22:37 +0300

    style: styles for the 404 pages

--- frontend/src/pages/notFound/NotFound.jsx
+++ frontend/src/pages/notFound/NotFound.jsx
@@ -1,18 +1,32 @@
-import React from 'react'
+import React from 'react';
 import { Link } from 'react-router-dom';
-// import './NotFound.css';
+import kitten404 from '../../assets/images/404-kitten.jpg';
+import '../../styles/notfound.css'; // Import the CSS file
 
 const NotFound = () => {
   return (
-    <div className="main-content">
-      <h1>404</h1>
-      <h2>Oops! Page Not Found</h2>
-      <p>It looks like the page you're looking for doesn't exist.</p>
-      <p>Maybe a cute pet picture will cheer you up!</p>
-      <img src="https://placekitten.com/400/300" alt="Cute kitten" className="not-found-image" />
-      <Link to="/" className="home-link">Go Back Home</Link>
+    <div className="main-container">
+      <div className="not-found-title">
+        <h1>404</h1>
+      </div>
+      <section className="not-found-container">
+        <div className="text-container">
+          <h2>Oops! Page Not Found</h2>
+          <section className='description'>
+            <p>It looks like the page you're looking for doesn't exist.</p>
+            <p>Maybe a cute pet picture will cheer you up!</p>
+          </section>
+        </div>
+        <div className="img-btn">
+          <img src={kitten404} alt="Cute kitten" className='not-found-image'/>
+          
+        </div>
+      </section>
+      <Link to="/" className="home-link">
+        <button>Go Back Home</button>
+      </Link>
     </div>
   );
 };
 
-export default NotFound
+export default NotFound;
