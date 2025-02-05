ec139802cdb843ea153c99603810a935081020ff
TKaburu <traceymwendwa@gmail.com>
2025-02-05 07:57:20 +0300

    feat: endpoint to get details of the current user

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('get-user/', getUser, name='user'),
 ]
--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from rest_framework import generics
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.response import Response
 from .models import CustomUser
 from .serializers import *
 
@@ -8,3 +10,13 @@ class RegisterView(generics.CreateAPIView):
     queryset = CustomUser.objects.all()
     serializer_class = CustomUserSerializer
     permission_classes = [AllowAny]
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def getUser(request):
+    """
+    Get the details of the currently logged-in user.
+    """
+    user = request.user
+    serializer = CustomUserSerializer(user)
+    return Response(serializer.data)
